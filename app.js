fd28fb2268ce5a0f9966cc69caf2389c703254b9
Abdus Sobur <34393556+AbdusSobur131@users.noreply.github.com>
2022-12-07 01:30:24 +0600

    Add files via upload

---
+++ app.js
@@ -0,0 +1,17 @@
+let age = 11;
+
+if(age < 18){
+    console.log("You are Young");
+}else if(age ==18){
+    console.log("Welcome to Adult");
+}else{
+    console.log("You are Matured")
+}
+
+const car=['Sobur','Madhurjo','Sayed','Shamim','Raihan'];
+
+
+for(let i=0;i<car.length;i++){
+    console.log (car[i]);
+}
+console.log("Well Done");
